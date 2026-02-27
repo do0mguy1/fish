ca3f6fe4bb06bf1752766c16b2f7110f0962bb72
do0mguy1 <Harley.Winsor@simplygreat.education>
2026-02-27 13:27:10 +0000

    made a fish

---
+++ Assets/scripts/fish.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class fish : MonoBehaviour
+{
+    
+    private SpriteRenderer render;
+    public float timer;
+    public float interval = 1f;
+    public float speed = 15;
+    public float lrspeed = 20;
+    public Rigidbody2D playerrb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            int randomNumber = Random.Range(0, 4);
+            timer = 0f;
+
+            if (randomNumber == 0)
+            {
+                playerrb.AddRelativeForce(Vector3.up * speed);
+                Debug.Log("w");
+            }
+
+            if (randomNumber == 1)
+            {
+                playerrb.AddRelativeForce(Vector3.left * lrspeed);
+                Debug.Log("a");
+                render.flipX = true;
+            }
+
+            if (randomNumber == 2)
+            {
+                playerrb.AddRelativeForce(Vector3.right * lrspeed);
+                Debug.Log("d");
+                render.flipX = false;
+            }
+
+            if (randomNumber == 3)
+            {
+                playerrb.AddRelativeForce(Vector3.down * speed);
+                Debug.Log("s");
+            }
+        }
+    }
+}
